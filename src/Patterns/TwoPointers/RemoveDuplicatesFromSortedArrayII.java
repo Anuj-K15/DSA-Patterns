14269ee0503b77d5fc24dcdc85ed9b438690f3d2
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2026-06-01 16:05:10 +0530

    RemoveDuplicatesFromSortedArrayII

---
+++ src/Patterns/TwoPointers/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,18 @@
+package Patterns.TwoPointers;
+
+public class RemoveDuplicatesFromSortedArrayII {
+    public static int removeDuplicates(int[] nums) {
+        int k = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+    }
+}
